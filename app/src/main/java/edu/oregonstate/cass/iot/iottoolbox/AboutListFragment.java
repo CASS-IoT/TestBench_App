cc50bcb1b6c96a4ff59bb52752fd45b7bde1df49
CASS-IoT <tyler.gilbert@oregonstate.edu>
2015-10-23 15:02:35 -0700

    Restarting the project from a different template, kept running into issues with the old branch.

---
+++ app/src/main/java/edu/oregonstate/cass/iot/iottoolbox/AboutListFragment.java
@@ -0,0 +1,22 @@
+package edu.oregonstate.cass.iot.iottoolbox;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AboutListFragment extends Fragment {
+
+    public AboutListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about_list, container, false);
+    }
+}
